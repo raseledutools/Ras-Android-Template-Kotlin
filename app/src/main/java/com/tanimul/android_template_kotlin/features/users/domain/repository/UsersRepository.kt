7fdeb56935e46cadd08918c5210a25bbfb9dea68
raseledutools <bhmrasel60@gmail.com>
2026-04-08 22:12:36 +0600

    Delete app/src/main/java/com/tanimul/android_template_kotlin/features/users directory

--- app/src/main/java/com/tanimul/android_template_kotlin/features/users/domain/repository/UsersRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.tanimul.android_template_kotlin.features.users.domain.repository
-
-import androidx.paging.PagingData
-import com.tanimul.android_template_kotlin.features.users.domain.model.User
-import kotlinx.coroutines.flow.Flow
-
-interface UsersRepository {
-    suspend fun fetchUsers(): Flow<PagingData<User>>
-}
