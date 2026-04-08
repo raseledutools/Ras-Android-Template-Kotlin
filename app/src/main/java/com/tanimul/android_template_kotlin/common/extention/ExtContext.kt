bb859b32f5f64ba61a816781946b2b97b84a0c8a
raseledutools <bhmrasel60@gmail.com>
2026-04-08 22:23:58 +0600

    Delete app/src/main/java/com/tanimul/android_template_kotlin/common directory

--- app/src/main/java/com/tanimul/android_template_kotlin/common/extention/ExtContext.kt
+++
@@ -1,8 +0,0 @@
-package com.tanimul.android_template_kotlin.common.extention
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
