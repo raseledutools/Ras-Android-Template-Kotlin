04f468e19f34fc7bed9d32eb0457df5221125fbb
raseledutools <bhmrasel60@gmail.com>
2026-04-08 23:01:26 +0600

    Delete app/src/main/java/com/tanimul/android_template_kotlin/AndroidTemplateApp.kt

--- app/src/main/java/com/tanimul/android_template_kotlin/AndroidTemplateApp.kt
+++
@@ -1,9 +0,0 @@
-package com.tanimul.android_template_kotlin.app
-
-import android.app.Application
-
-class AndroidTemplateApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
