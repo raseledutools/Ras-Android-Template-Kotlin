04b755eccd9a8158e0631da83f3c9432e65d90ca
raseledutools <bhmrasel60@gmail.com>
2026-04-08 15:58:53 +0600

    Create Color.kt

---
+++ app/src/main/java/com/tanimul/android_template_kotlin/features/Color.kt
@@ -0,0 +1,10 @@
+package com.tanimul.android_template_kotlin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF1E2428)
+val AppBackground = Color(0xFFF0F0F0)
+val SectionBlue = Color(0xFF4285F4)
+val SectionRed = Color(0xFFFF5252)
+val SectionGreen = Color(0xFF4CAF50)
+val SwitchGreenON = Color(0xFF4CAF50)
