e96c85eece8798d23aafd2764a2c395cd4390cf3
raseledutools <bhmrasel60@gmail.com>
2026-04-08 22:22:29 +0600

    Create BlockerHeroViewModel.kt

---
+++ app/src/main/java/com/tanimul/android_template_kotlin/features/BlockerHeroViewModel.kt
@@ -0,0 +1,7 @@
+package com.tanimul.android_template_kotlin.features
+
+import androidx.lifecycle.ViewModel
+
+class BlockerHeroViewModel : ViewModel() {
+    // আপাতত এটি খালি, ডিজাইনের জন্য এর বেশি কিছু লাগবে না
+}
