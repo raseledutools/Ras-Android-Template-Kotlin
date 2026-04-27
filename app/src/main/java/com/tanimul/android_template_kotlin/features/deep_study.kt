b53a7150c3d3463d7d51f10b8ad61a09f5c5c053
raseledutools <bhmrasel60@gmail.com>
2026-04-27 21:27:24 +0600

    Update deep_study.kt

--- app/src/main/java/com/tanimul/android_template_kotlin/features/deep_study.kt
+++ app/src/main/java/com/tanimul/android_template_kotlin/features/deep_study.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
